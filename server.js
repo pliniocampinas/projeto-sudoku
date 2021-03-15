6004da64d37b35abd883637eb7889371e0134e57
unknown <plinio.lsc@gmail.com>
2021-03-14 22:34:28 -0300

    func - implementing solver with pure js, sudoku-solver.py is now deleted, no spawn process is used anymore

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
-const http = require('http');
-const sudokuSolver = require('./sudoku-solver');
-const url = require('url');
+const http = require('http')
+const sudokuSolver = require('./sudoku-solver')
+const url = require('url')
 
 http.createServer(function (request, response) {
-    console.log('request ', request.url);
+    console.log('request.url ', request.url)
+    const searchParams = new url.URL(request.url, 'https://dummyurl.com').searchParams
+    const linearTable = searchParams?.get('table')?.split(',')
+    if(!linearTable || linearTable.length < 81) {
+        response.writeHead(400, { 'Access-Control-Allow-Origin': '*'})
+        response.end({error: 'Bad sudoku table'}, 'utf-8')
+    }
+    const maxResults = searchParams.get('maxResults')
+    solver = new sudokuSolver.Solver()
 
-    let requestData = url.parse(request.url, true).query;
-    let linearTable = requestData.table;
-    let maxResults = requestData.maxResults;
-    sudokuSolver.getTableSolved(linearTable, maxResults, (solution, status) => {
-        response.writeHead(status, { 'Access-Control-Allow-Origin': '*'});
-        console.log(solution);
-        response.end(solution, 'utf-8');
-    });
-
-}).listen(8125);
-console.log('Server running at http://127.0.0.1:8125/');
+    solver.setMaxResults(maxResults)
+    solver.setBoard(linearTable)
+    console.log('solving the sudoku')
+    solver.solve()
+    console.log('solved')
+    const results = solver.getResults()
+    response.writeHead(200, { 'Access-Control-Allow-Origin': '*'})
+    response.end(JSON.stringify({'results': results}), 'utf-8')
+}).listen(8125)
+console.log('Server running at http://127.0.0.1:8125/')
--- sudoku-solver.js
+++ sudoku-solver.js
@@ -1,26 +1,96 @@
-const spawn = require("child_process").spawn;
+class Solver {
 
-function getTableSolved(linearTable, maxResults, callback) {
-    const pythonProcess = spawn('python',["sudoku-solver.py", linearTable, maxResults]);
-    
-    var status = 1;
-    var dataStr = '';
-    pythonProcess.stdout.on('data', (data) => {
-        // console.log("data.toString");
-        // console.log(data.toString());
-        dataStr = data.toString();
-        status = 200;
-        callback(dataStr, status);
-    });
+    constructor() {
+        this.max_results = 0
+        this.n_results = 0
+        this.solverResults  = []
+        this.sudoku_board = null
+        this.initBoard()
+    }
 
-    pythonProcess.stderr.on('data',(data)=>{
-        //Here data is of type buffer
-        console.log("data.toString err");
-        console.log(data.toString())
-        dataStr = data.toString();
-        status = 500;
-        callback(dataStr, status);
-    })
+    initBoard() {
+        this.sudoku_board = []
+        for(var i=0; i<9; i++) {
+            this.sudoku_board[i] = [];
+            for(var j=0; j<9; j++) {
+                this.sudoku_board[i][j] = 0
+            }
+        }
+    }
+
+    setBoard(linearTable) {
+        linearTable.forEach((cell, index) => {
+            let x = index % 9
+            let y = (Math.floor(index/9))
+            this.sudoku_board[y][x] = cell * 1
+        })
+    }
+
+    getResults() {
+        return this.solverResults
+    }
+
+    setMaxResults(maxResults) {
+        this.max_results = maxResults
+    }
+
+    isAPossibleMove(yPosition, xPosition, moveNumber) {
+        // global sudoku_board
+        for(let i=0; i < 9; i++) {
+            if (this.sudoku_board[yPosition][i] == moveNumber) {
+                return false
+            }
+        }
+
+        for(let i=0; i < 9; i++) {
+            if (this.sudoku_board[i][xPosition] == moveNumber) {
+                return false
+            }
+        }
+            
+        let x0 = Math.floor(xPosition/3) * 3
+        let y0 = Math.floor(yPosition/3) * 3
+
+        for(var i=0; i<3; i++) {
+            for(var j=0; j<3; j++) {
+                if(this.sudoku_board[y0+i][x0+j] == moveNumber) {
+                    return false
+                }
+            }
+        }
+
+        return true
+    }
+
+    solve() {
+        for(var x=0; x<9; x++) {
+            for(var y=0; y<9; y++) {
+
+                if(this.sudoku_board[y][x] == 0) {
+
+                    for(var n=1; n<10; n++) {
+                        if(this.isAPossibleMove(y,x,n)) {
+                            this.sudoku_board[y][x] = n
+                            this.n_results = this.solve()
+                            if(this.n_results == this.max_results) {
+                                return this.n_results
+                            }
+                            this.sudoku_board[y][x] = 0
+                        }
+                    }
+                    return this.n_results
+                }
+
+            }
+        }
+
+        this.solverResults.push(this.deepCopy(this.sudoku_board))
+        return this.n_results + 1
+    }
+
+    deepCopy(matrix) {
+        return matrix.map(line => line.map(cell => cell))
+    }
 }
 
-module.exports.getTableSolved = getTableSolved
+module.exports.Solver = Solver
